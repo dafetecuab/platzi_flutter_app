9f4b50e2dd9e8587b5fd2ce0fb501d009ca29191
Alejandro Fetecua Bernal <fetecua96@gmail.com>
2021-11-29 09:53:42 -0500

    Initial commit

---
+++ lib/description_place.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Description_place extends StatelessWidget {
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  Description_place(this.namePlace, this.stars, this.descriptionPlace, {Key? key}) : super(key: key);
+
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final star_half = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+    final star_border = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+    final title_stars = Row(
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0
+          ),
+          child: Text(
+            namePlace,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: [
+            star,
+            star,
+            star,
+            star,
+            star_border,
+          ],
+        )
+      ],
+    );
+    final description = Container(
+      margin: new EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0,
+      ),
+      child: Text(
+        descriptionPlace ,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87
+        ),
+      ),
+    );
+    return Column(
+      children: [
+        title_stars,
+        description
+      ],
+    );
+  }
+  
+}
